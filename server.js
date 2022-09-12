4f46a0629deb0c8da753393f1ee2abf679981fef
Marcus Pruks <mapruk@ttu.ee>
2022-09-12 23:57:06 +0300

    test2

--- server.js
+++ server.js
@@ -1,36 +1,18 @@
-/*const express = require('express')
+const express = require('express');
+const app = express();
 
-const app = express()
 
-
-let encodeUrl = express.urlencoded({ extended: false })
+let encodeUrl = express.urlencoded({ extended: false });
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
-})
+});
 
 app.post('/', encodeUrl, (req, res) => {
     console.log('Form request:', req.body)
     res.send("Tere, " + req.body.input)
-})
-
-app.listen(8080)
-
-const port = 8080
-const host = '127.0.0.1'
-console.log(`Listening at http://${host}:${port}`)**/
-
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res
-        .status(200)
-        .send('Hello server is running')
-        .end();
 });
 
-// Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
